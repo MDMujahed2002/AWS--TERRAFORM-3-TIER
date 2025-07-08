b067ca1949d841c577c2fc8aac4d46f6ff617fd0
MDMujahed2002 <mmujaheed888@gmail.com>
2025-07-08 12:53:20 +0530

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -5,7 +5,6 @@ resource "aws_db_instance" "my_rds" {
   allocated_storage       = 20
   username                = var.rds_username
   password                = var.rds_password
-  db_subnet_group_name    = aws_db_subnet_group.rds_subnet_group.name
   vpc_security_group_ids  = [aws_security_group.rds_sg.id]
   skip_final_snapshot     = true
   publicly_accessible     = false
